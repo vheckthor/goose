35f429437ddf6cbcfa6462fa8e5027c866055d07
Wendy Tang <wendytang@squareup.com>
2024-12-06 13:58:03 -0800

    Update Datadog config with direct values

--- ui/desktop/src/datadog-config.js
+++ ui/desktop/src/datadog-config.js
@@ -1,12 +1,16 @@
 import { datadogRum } from '@datadog/browser-rum';
 
+const DATADOG_APPLICATION_ID = '139d4946-a1a5-4d5f-b017-2422e7774b9d';
+const DATADOG_CLIENT_TOKEN = 'puba9048a40434f456895695b2d552b9c5c';
+const DATADOG_ENV = 'dev';
+
 export const initDatadog = () => {
     datadogRum.init({
-        applicationId: process.env.DATADOG_APPLICATION_ID,
-        clientToken: process.env.DATADOG_CLIENT_TOKEN,
+        applicationId: DATADOG_APPLICATION_ID,
+        clientToken: DATADOG_CLIENT_TOKEN,
         site: 'datadoghq.com',
         service: 'goose',
-        env: process.env.DATADOG_ENV || 'dev',
+        env: DATADOG_ENV,
         sessionSampleRate: 100,
         sessionReplaySampleRate: 20,
         trackUserInteractions: true,
