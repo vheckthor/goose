7beb85b0dd6e9df93266ce133f1e999a5aadf080
Jarrod Sibbison <jsibbison@squareup.com>
2024-12-18 14:11:59 +1100

    relayout tui-configure

--- crates/goose-cli/src/commands/tui_configure.rs
+++ crates/goose-cli/src/commands/tui_configure.rs
@@ -1,12 +1,12 @@
 use std::{
-    cmp::max, io::{self, stdout}, panic::{set_hook, take_hook}, vec
+    cmp::max, collections::HashMap, io::{self, stdout}, panic::{set_hook, take_hook}, vec
 };
 
 use console::Key;
 use ratatui::{
     backend::{Backend, CrosstermBackend}, crossterm::{
-        event::{self, Event, KeyCode}, execute, terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
-    }, layout::{self, Layout}, style::{Modifier, Style}, text::{Line, Span, Text}, widgets::{Block, Borders, HighlightSpacing, List, ListState, Paragraph}, Frame, Terminal
+        event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers}, execute, terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+    }, layout::{self, Layout}, style::{Color, Modifier, Style}, text::{Line, Span, Text}, widgets::{Block, Borders, HighlightSpacing, List, ListState, Paragraph}, Frame, Terminal
 };
 
 use crate::{main, profile::{load_profiles, Profile}};
@@ -43,8 +43,29 @@ fn restore_tui() -> io::Result<()> {
 struct ConfigureState {
     ui_mode: UIMode,
     profile_list_state: ListState,
-    profiles: Vec<(String, Profile)>,
-    edit_profile: Option<(String, Profile)>,
+    profiles: HashMap<String, Profile>,
+}
+
+#[derive(Clone)]
+struct EditableProfile {
+    pub focussed_field: InputField,
+    // pub name: TextState<'a>,
+}
+
+impl EditableProfile {
+    fn new(name: &String, profile: &Profile) -> Self {
+        Self {
+            focussed_field: InputField::Name,
+            // name: TextState::new().with_value(name.clone()),
+        }
+    }
+}
+
+#[derive(Clone)]
+enum InputField {
+    Name,
+    // Provider,
+    // Model,
 }
 
 enum UIMode {
@@ -57,8 +78,7 @@ impl ConfigureState {
         let mut state= Self { 
             ui_mode: UIMode::ProfileView, 
             profile_list_state: ListState::default(), 
-            profiles: load_profiles().unwrap().into_iter().collect(),
-            edit_profile: None,
+            profiles: load_profiles().unwrap(),
         };
         if state.profiles.len() > 0 {
             state.profile_list_state.select_first();
@@ -67,125 +87,194 @@ impl ConfigureState {
     }
 }
 
-async fn run(mut tui: Terminal<impl Backend>) -> io::Result<()> {
-    let mut ui_state = ConfigureState::new();
+enum AppOutcome {
+    Continue,
+    Exit,
+}
 
-    loop {
-        let mut profile_list_names: Vec<String> = ui_state.profiles.iter().map(|(name, _)| name.clone()).collect();
-        let has_profiles = profile_list_names.len() > 0;
-        profile_list_names.sort();
-        tui.draw(|f| {
-            // Fit all the profile items and enough room to display their details including systems, just using dummy 14 for now.
-            let profile_view_height = max(14, ui_state.profiles.len() + 4) as u16;
+struct App {
+    ui_state: ConfigureState,
+    edit_profile: Option<EditableProfile>,
+}
 
-            let vertical_chunks = Layout::default()
-                .direction(layout::Direction::Vertical)
-                // header, context title (Profiles), main display, footer
-                .constraints([layout::Constraint::Length(1), layout::Constraint::Length(3), layout::Constraint::Length(profile_view_height), layout::Constraint::Min(1), layout::Constraint::Min(1)])
-                .split(f.area());
-
-            let main_area = vertical_chunks[2];
-            let footer_area = vertical_chunks[3];
-
-            render_header(f, vertical_chunks[0]);
-
-            let profiles_title = Span::styled("Profiles", Style::default().add_modifier(Modifier::UNDERLINED));
-            let systems_title = Span::styled("Systems (todo)", Style::default()); // Update modifier when selected
-            f.render_widget(Text::from(vec!
-                [Line::from(""),
-                Line::from(vec![Span::raw("   "), profiles_title, Span::raw("   "), systems_title]),
-                Line::from(vec![Span::raw("─".repeat((f.area().width as usize).saturating_sub(24)))]),
-                Line::from(""),
-                ]
-            ), vertical_chunks[1]);
-
-            // Main area
-            let main_area_horizontal_chunks = Layout::default()
-                .direction(layout::Direction::Horizontal)
-                .constraints([layout::Constraint::Length(33), layout::Constraint::Min(1)])
-                .split(main_area);
-
-            // Main - Profile list area
-            let profile_list_chunks = Layout::default()
-                .direction(layout::Direction::Vertical)
-                .constraints([layout::Constraint::Length(2), layout::Constraint::Min(1)])
-                .split(main_area_horizontal_chunks[0]);
-
-            let profile_list_header = Paragraph::new(vec![
-                Line::from(vec![Span::styled("   Profiles List", Style::default().add_modifier(Modifier::ITALIC))]),
-                Line::from(vec!["".into()])
-            ]);
-            f.render_widget(profile_list_header, profile_list_chunks[0]);
-
-            
-            let profile_list = List::new(profile_list_names.clone())
-                .block(Block::default().borders(Borders::NONE))
-                .highlight_symbol(" > ")
-                .highlight_spacing(HighlightSpacing::Always);
-            f.render_stateful_widget(profile_list, profile_list_chunks[1], &mut ui_state.profile_list_state);
-
-            // Main - Profile details area
-            match ui_state.ui_mode {
-                UIMode::ProfileView => {
-                    if has_profiles {
-                        let (selected_profile_name, selected_profile) = ui_state.profiles.get(ui_state.profile_list_state.selected().unwrap_or(0)).unwrap();
-                        let profile_view = Paragraph::new(vec![
-                            Line::from(vec![Span::styled("    Profile Details", Style::default().add_modifier(Modifier::ITALIC))]),
-                            Line::from(vec!["".into()]),
-                            Line::from(vec!["    Name:         ".into(), selected_profile_name.clone().into()]),
-                            Line::from(vec!["    Provider:     ".into(), selected_profile.provider.clone().into()]),
-                            Line::from(vec!["    Model:        ".into(), selected_profile.model.clone().into()]),
-                        ]).block(Block::default().borders(Borders::LEFT));
-                        f.render_widget(profile_view, main_area_horizontal_chunks[1]);
-                    } else {
-                        let profile_view = Paragraph::new(vec![
-                            Line::from(vec![Span::styled("    Profile Details", Style::default().add_modifier(Modifier::ITALIC))]),
-                            Line::from(vec!["".into()]),
-                            Line::from(vec!["    Create a New Profile".into()]),
-                        ]).block(Block::default().borders(Borders::LEFT));
-                        f.render_widget(profile_view, main_area_horizontal_chunks[1]);
-                    }
-                },
-                UIMode::ProfileEdit => {
-                    let edit_section_chunks = Layout::default()
-                        .direction(layout::Direction::Vertical)
-                        .constraints([layout::Constraint::Length(2), layout::Constraint::Min(1)])
-                        .split(main_area_horizontal_chunks[1]);
-
-                    // TODO: Implement editing profile
-                }
-            }
-
-            // Footer
-            render_footer(f, footer_area);
-        })?;
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => {
-                    // TODO: Add confirmation dialog
-                    restore_tui()?;
-                    break;
-                }
-                KeyCode::Char('e') => {
-                    if has_profiles {
-                        ui_state.ui_mode = UIMode::ProfileEdit;
-                        ui_state.edit_profile = Some(ui_state.profiles.get(ui_state.profile_list_state.selected().unwrap_or(0)).unwrap().clone());
-                    }
-                }
-                KeyCode::Down => {
-                    ui_state.profile_list_state.select_next();
-                }
-                KeyCode::Up => {
-                    ui_state.profile_list_state.select_previous();
-
-                }
-                _ => {}
-            }
+impl App {
+    fn new() -> Self {
+        Self {
+            ui_state: ConfigureState::new(),
+            edit_profile: None,
         }
     }
 
+    fn draw(&mut self, f: &mut Frame) {
+        let profile_list_names: Vec<String> = profile_list_names(&self.ui_state.profiles);
+        let has_profiles: bool = profile_list_names.len() > 0;
+
+        // Fit all the profile items and enough room to display their details including systems, just using dummy 14 for now.
+        let profile_view_height = max(14, self.ui_state.profiles.len() + 4) as u16;
+
+        let vertical_chunks = Layout::default()
+            .direction(layout::Direction::Vertical)
+            // header, context title (Profiles), main display, footer
+            .constraints([layout::Constraint::Length(1), layout::Constraint::Length(3), layout::Constraint::Length(profile_view_height), layout::Constraint::Min(1), layout::Constraint::Min(1)])
+            .split(f.area());
+
+        let main_area = vertical_chunks[2];
+        let footer_area = vertical_chunks[3];
+
+        render_header(f, vertical_chunks[0]);
+
+        let profiles_title = Span::styled("Profiles", Style::default().add_modifier(Modifier::UNDERLINED));
+        let systems_title = Span::styled("Systems (todo)", Style::default()); // Update modifier when selected
+        f.render_widget(Text::from(vec!
+            [Line::from(""),
+            Line::from(vec![Span::raw("   "), profiles_title, Span::raw("   "), systems_title]),
+            Line::from(vec![Span::raw("─".repeat((f.area().width as usize).saturating_sub(24)))]),
+            Line::from(""),
+            ]
+        ), vertical_chunks[1]);
+
+        // Main area
+        let main_area_horizontal_chunks = Layout::default()
+            .direction(layout::Direction::Horizontal)
+            .constraints([layout::Constraint::Length(33), layout::Constraint::Min(1)])
+            .split(main_area);
+
+        // Main - Profile list area
+        let profile_list_chunks = Layout::default()
+            .direction(layout::Direction::Vertical)
+            .constraints([layout::Constraint::Length(2), layout::Constraint::Min(1)])
+            .split(main_area_horizontal_chunks[0]);
+
+        let profile_list_header = Paragraph::new(vec![
+            Line::from(vec![Span::styled("   Profiles List", Style::default().add_modifier(Modifier::ITALIC))]),
+            Line::from(vec!["".into()])
+        ]);
+        f.render_widget(profile_list_header, profile_list_chunks[0]);
+
+        
+        let profile_list = List::new(profile_list_names.clone())
+            .block(Block::default().borders(Borders::NONE))
+            .highlight_symbol(" > ")
+            .highlight_spacing(HighlightSpacing::Always);
+        f.render_stateful_widget(profile_list, profile_list_chunks[1], &mut self.ui_state.profile_list_state);
+
+        // Main - Profile details area
+        match self.ui_state.ui_mode {
+            UIMode::ProfileView => {
+                if has_profiles {
+                    let (selected_profile_name, selected_profile) = selected_profile(&self.ui_state, &profile_list_names).unwrap();
+                    let profile_view = Paragraph::new(vec![
+                        Line::from(vec![Span::styled("    Profile Details", Style::default().add_modifier(Modifier::ITALIC))]),
+                        Line::from(vec!["".into()]),
+                        Line::from(vec!["    Name:         ".into(), selected_profile_name.clone().into()]),
+                        Line::from(vec!["    Provider:     ".into(), selected_profile.provider.clone().into()]),
+                        Line::from(vec!["    Model:        ".into(), selected_profile.model.clone().into()]),
+                    ]).block(Block::default().borders(Borders::LEFT));
+                    f.render_widget(profile_view, main_area_horizontal_chunks[1]);
+                } else {
+                    let profile_view = Paragraph::new(vec![
+                        Line::from(vec![Span::styled("    Profile Details", Style::default().add_modifier(Modifier::ITALIC))]),
+                        Line::from(vec!["".into()]),
+                        Line::from(vec!["    Create a New Profile".into()]),
+                    ]).block(Block::default().borders(Borders::LEFT));
+                    f.render_widget(profile_view, main_area_horizontal_chunks[1]);
+                }
+            },
+            UIMode::ProfileEdit => {
+                let edit_section_chunks = Layout::default()
+                    .direction(layout::Direction::Vertical)
+                    .constraints([layout::Constraint::Length(2), layout::Constraint::Min(1)])
+                    .split(main_area_horizontal_chunks[1]);
+
+                // TODO: Implement editing profile
+                let edit_header = Paragraph::new(vec![
+                    Line::from(vec![Span::styled("    Edit Profile", Style::default().add_modifier(Modifier::ITALIC))]),
+                    Line::from(vec!["".into()]),
+                ]).block(Block::default().borders(Borders::LEFT));
+
+                f.render_widget(edit_header, edit_section_chunks[0]);
+
+                // TODO: Render editable fields
+            }
+        }
+
+        // Footer
+        render_footer(f, footer_area);
+    }
+
+    fn handle_events(&mut self) -> io::Result<AppOutcome> {
+        if let Event::Key(key) = event::read()? {
+            match self.ui_state.ui_mode {
+                UIMode::ProfileView => {
+                    match key {
+                        KeyEvent { code: KeyCode::Char('c'), modifiers: KeyModifiers::CONTROL, kind: KeyEventKind::Press, state: KeyEventState::NONE } => {
+                            return Ok(AppOutcome::Exit);
+                        }
+                        _ => {}
+                    }
+                    match key.code {
+                        KeyCode::Char('q') => {
+                            return Ok(AppOutcome::Exit);
+                        }
+                        KeyCode::Char('e') => {
+                            if has_profiles(&self.ui_state.profiles) {
+                                self.ui_state.ui_mode = UIMode::ProfileEdit;
+                                let profile_names = profile_list_names(&self.ui_state.profiles);
+                                let (name, profile) = selected_profile(&self.ui_state, &profile_names).unwrap();
+                                self.edit_profile = Some(EditableProfile::new(name, profile));
+                            }
+                        }
+                        KeyCode::Down => {
+                            self.ui_state.profile_list_state.select_next();
+                        }
+                        KeyCode::Up => {
+                            self.ui_state.profile_list_state.select_previous();
+                        }
+                        _ => {}
+                    }
+                }
+                UIMode::ProfileEdit => {
+                    match key.code {
+                        KeyCode::Esc => {
+                            self.ui_state.ui_mode = UIMode::ProfileView;
+                            self.edit_profile = None;
+                        },
+                        KeyCode::Enter => {
+                            // Handle submit
+                        }
+                        // Handle up/down arrow keys
+                        _ => {
+                            // match self.edit_profile.unwrap().focussed_field {
+                            //     InputField::Name => {
+                            //         self.edit_profile.unwrap().name.handle_key_event(KeyEvent::from(key));
+                            //     }
+                            // }
+                        }
+                    }
+                },
+            }
+        }
+        Ok(AppOutcome::Continue)
+    }
+}
+
+async fn run(mut tui: Terminal<impl Backend>) -> io::Result<()> {
+    let mut app = App::new();
+    loop {
+        tui.draw(|f| app.draw(f) )?;
+        match app.handle_events() {
+            Ok(AppOutcome::Continue) => continue,
+            Ok(AppOutcome::Exit) => break,
+            Err(e) => break,
+        }
+    }
+    restore_tui()?;
     Ok(())
+}
+
+fn selected_profile<'a>(ui_state: &'a ConfigureState, profile_list_names: &'a Vec<String>) -> Option<(&'a String, &'a Profile)> {
+    let target_profile_name = profile_list_names.get(ui_state.profile_list_state.selected().unwrap_or(0)).unwrap();
+    Some(ui_state.profiles.iter().find(|(name, _)| target_profile_name == *name).map(|(name, profile)| (name, profile)).unwrap())
 }
 
 fn render_header(f: &mut Frame, header_area: layout::Rect) {
@@ -220,4 +309,14 @@ fn render_footer(f: &mut Frame, footer_area: layout::Rect) {
         ])
     ]);
     f.render_widget(Paragraph::new(footer).block(Block::new()), footer_area);
+}
+
+fn has_profiles(profiles: &HashMap<String, Profile>) -> bool {
+    profiles.len() > 0
+}
+
+fn profile_list_names(profiles: &HashMap<String, Profile>) -> Vec<String> {
+    let mut strs: Vec<String> = profiles.iter().map(|(name, _)| name.clone()).collect();
+    strs.sort();
+    strs
 }
