8cebd7e4bcf1fadc442ca7916bc01875e669c057
Wendy Tang <wendytang@squareup.com>
2025-01-10 16:21:43 -0800

    check in draft

--- crates/goose-mcp/src/lib.rs
+++ crates/goose-mcp/src/lib.rs
@@ -1,9 +1,11 @@
 mod developer;
 mod developer2;
 mod jetbrains;
+mod memory;
 mod nondeveloper;
 
 pub use developer::DeveloperRouter;
 pub use developer2::Developer2Router;
 pub use jetbrains::JetBrainsRouter;
+pub use memory::MemoryRouter;
 pub use nondeveloper::NonDeveloperRouter;
