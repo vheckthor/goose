7850fc4312fe3b813386be1bc2ed5b24f9e9f773
Nahiyan Khan <nahiyan.khan@gmail.com>
2025-01-24 12:07:49 -0500

    Landing page [first pass] (#732)

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -1,56 +1,57 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'codename goose',
-  tagline: 'goose is your on-machine developer agent, automating engineering tasks seamlessly within your IDE or terminal',
-  favicon: 'img/favicon.ico',
+  title: "codename goose",
+  tagline:
+    "goose is your on-machine developer agent, automating engineering tasks seamlessly within your IDE or terminal",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://block.github.io/',
+  url: "https://block.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/goose/v1/', // This is temporary for development purposes
+  baseUrl: "/goose/v1/", // This is temporary for development purposes
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'block', // Usually your GitHub org/user name.
-  projectName: 'goose', // Usually your repo name.
+  organizationName: "block", // Usually your GitHub org/user name.
+  projectName: "goose", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -58,82 +59,90 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/home-banner.png',
+    image: "img/home-banner.png",
     navbar: {
-      title: 'codename goose',
+      title: "",
       logo: {
-        alt: 'Block Logo',
-        src: 'img/logo.png',
+        alt: "Block Logo",
+        src: "img/logo_light.png",
+        srcDark: "img/logo_dark.png",
       },
       items: [
         {
-          to: '/docs/quickstart',
-          label: 'Quickstart', 
-          position: 'left',
+          to: "/docs/quickstart",
+          label: "Quickstart",
+          position: "left",
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
-        
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {href: 'https://discord.gg/block-opensource', label: 'Discord', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/block/goose',
-          label: 'GitHub',
-          position: 'right',
+          to: "https://block.github.io/goose/v1/extensions/",
+          label: "Extensions",
+          position: "left",
+        },
+        {
+          href: "https://discord.gg/block-opensource",
+          label: "Discord",
+          position: "right",
+        },
+        {
+          href: "https://github.com/block/goose",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Install goose',
-              to: '/docs/installation',
+              label: "Install goose",
+              to: "/docs/installation",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discord.gg/block-opensource',
+              label: "Discord",
+              href: "https://discord.gg/block-opensource",
             },
             {
-              label: 'YouTube',
-              href: 'https://www.youtube.com/@goose.videos',
+              label: "YouTube",
+              href: "https://www.youtube.com/@goose.videos",
             },
             {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/block-opensource',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/block-opensource",
             },
             {
-              label: 'Twitter / X',
-              href: 'https://x.com/blockopensource',
-            },         
+              label: "Twitter / X",
+              href: "https://x.com/blockopensource",
+            },
             {
-              label: 'BlueSky',
-              href: 'https://bsky.app/profile/block-opensource.bsky.social',
-            },                
+              label: "BlueSky",
+              href: "https://bsky.app/profile/block-opensource.bsky.social",
+            },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              to: "/blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/block/goose',
+              label: "GitHub",
+              href: "https://github.com/block/goose",
             },
           ],
         },
--- documentation/src/components/HomepageFeatures/index.tsx
+++ documentation/src/components/HomepageFeatures/index.tsx
@@ -1,18 +1,18 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Open Source",
+    Svg: require("@site/static/img/lock-unlocked-fill.svg").default,
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -21,8 +21,8 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Runs Locally",
+    Svg: require("@site/static/img/category-moving.svg").default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -31,8 +31,18 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Extensible",
+    Svg: require("@site/static/img/category-ETF.svg").default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+  {
+    title: "Autonomous",
+    Svg: require("@site/static/img/pay-in-four.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -42,13 +52,13 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    <div className={clsx("col col--3")}>
+      <div className="text--left padding-horiz--md">
+        <Svg className={styles.featureIcon} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text--left padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
