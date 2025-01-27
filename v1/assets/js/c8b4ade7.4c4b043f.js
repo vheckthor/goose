1fc183269e249f4cbe1a972b8d0dbf1dedaf825d
acekyd <acekyd@users.noreply.github.com>
2025-01-27 21:39:21 +0000

    deploy: b41dd26908d65e68feb94b7cff88ea32017c5b58

---
+++ v1/assets/js/c8b4ade7.4c4b043f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgoose=self.webpackChunkgoose||[]).push([[127],{1322:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/goose/v1/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
