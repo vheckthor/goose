6c201cdaa23739eec7c01cc037420f4803ad6d1b
Salman Mohammed <smohammed@squareup.com>
2025-03-07 09:05:35 -0500

    session working dir is working as expected

--- ui/desktop/src/components/ChatView.tsx
+++ ui/desktop/src/components/ChatView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState, useMemo } from 'react';
 import { getApiUrl } from '../config';
-import { generateSessionId, updateSessionMetadata } from '../sessions';
+import { generateSessionId } from '../sessions';
 import BottomMenu from './BottomMenu';
 import FlappyGoose from './FlappyGoose';
 import GooseMessage from './GooseMessage';
