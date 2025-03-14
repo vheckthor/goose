6602384f802daf78d3eb40f08ebe1ddc5d18f618
Rizel Scarlett <rizel@squareup.com>
2025-03-14 00:46:01 -0400

    switching to hashrouter

--- prompt-library/app/router.tsx
+++ prompt-library/app/router.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import HomePage from "./routes/home";
 import DetailPage from "./routes/detail";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <HomePage />,
