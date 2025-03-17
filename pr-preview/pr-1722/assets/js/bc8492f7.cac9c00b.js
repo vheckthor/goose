456e552c47019b33817c3a1f3b2eb4550233b13b
salman1993 <salman1993@users.noreply.github.com>
2025-03-17 13:47:56 +0000

    deploy: af2e52dfc03621fe7450d072696a09ad19b23b25

--- pr-preview/pr-1722/assets/js/bc8492f7.cac9c00b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgoose=self.webpackChunkgoose||[]).push([[8078],{348:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/goose/pr-preview/pr-1722/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":11,"nextPage":"/goose/pr-preview/pr-1722/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
