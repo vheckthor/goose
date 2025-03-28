73fdb1eb9a9059fdf0b93d86b67c03ff26bba6dd
Michael Neale <michael.neale@gmail.com>
2025-03-28 14:08:53 +1100

    fixing up the bad merge

--- ui/desktop/src/components/ChatView.tsx
+++ ui/desktop/src/components/ChatView.tsx
@@ -392,19 +392,16 @@ export default function ChatView({
   }, [filteredMessages, isUserMessage]);
 
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center">
-      <div className="relative flex items-center h-[36px] w-full">
-        <MoreMenuLayout setView={setView} setIsGoosehintsModalOpen={setIsGoosehintsModalOpen}
-          toggleWebView={() => setShowWebView(!showWebView)} />
-      </div>
-
-      <Card className="flex flex-col flex-1 rounded-none h-[calc(100vh-95px)] w-full bg-bgApp mt-0 border-none relative">
-        {messages.length === 0 ? (
-          <Splash
-            append={(text) => append(createUserMessage(text))}
-            activities={botConfig?.activities || null}
+    <div className="flex w-full h-screen">
+      <div className="flex flex-col h-screen flex-1">
+        <div className="relative flex items-center h-[36px] w-full">
+          <MoreMenuLayout
+            setView={setView}
+            setIsGoosehintsModalOpen={setIsGoosehintsModalOpen}
+            toggleWebView={() => setShowWebView(!showWebView)}
           />
         </div>
+
         <Card className="flex flex-col flex-1 rounded-none h-[calc(100vh-95px)] w-full bg-bgApp mt-0 border-none relative">
           {messages.length === 0 ? (
             <Splash
