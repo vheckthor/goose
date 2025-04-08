3e41192813a2a966bfdc427b5ebed6d9ba4a07ed
blackgirlbytes <blackgirlbytes@users.noreply.github.com>
2025-04-08 13:08:02 +0000

    deploy: 76e2686c7432a5e3c9c5e1112757b3b663f0a798

--- assets/js/a8538bae.ccfc6e44.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgoose=self.webpackChunkgoose||[]).push([[5680],{4319:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/goose/blog/page/2","page":2,"postsPerPage":10,"totalPages":3,"totalCount":22,"previousPage":"/goose/blog","nextPage":"/goose/blog/page/3","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
