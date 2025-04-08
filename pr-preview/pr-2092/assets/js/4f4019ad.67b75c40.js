68e43b382ff3eb1468803ff74687aee7c896be03
Kvadratni <Kvadratni@users.noreply.github.com>
2025-04-08 19:54:37 +0000

    deploy: 8b5ff8a4a9d2bef2d6e537bbc667f5179eb5f813

--- pr-preview/pr-2092/assets/js/4f4019ad.67b75c40.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgoose=self.webpackChunkgoose||[]).push([[192],{3965:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/goose/pr-preview/pr-2092/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":23,"nextPage":"/goose/pr-preview/pr-2092/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
