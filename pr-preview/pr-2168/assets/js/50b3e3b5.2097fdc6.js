77d09b72edbd7dfa98ba8d95a7f915dbf583217a
taniashiba <taniashiba@users.noreply.github.com>
2025-04-11 22:03:36 +0000

    Deploy preview for PR 2168 🛫

---
+++ pr-preview/pr-2168/assets/js/50b3e3b5.2097fdc6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgoose=self.webpackChunkgoose||[]).push([[4474],{1397:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/goose/pr-preview/pr-2168/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":25,"nextPage":"/goose/pr-preview/pr-2168/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
