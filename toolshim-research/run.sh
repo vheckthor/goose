cbb79e74a259f9c46b0d8b9ad20f140c2895cfdb
Michael Neale <michael.neale@gmail.com>
2025-04-15 19:45:41 +1000

    write to file

--- toolshim-research/run.sh
+++ toolshim-research/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -rf out-chat out-listing
-mkdir -p out-chat out-listing
+rm -rf out-chat out-listing out-create-file
+mkdir -p out-chat out-listing out-create-file
 # 
 ./ollama_tester.py --query "what can you do?" --output "out-chat"
 ./ollama_tester.py --query "list files in this dir" --output "out-listing"
+./ollama_tester.py --query "can you make a file hello.py and put a simple hello world app in it" --output "out-create-file"
