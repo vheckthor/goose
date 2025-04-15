dc2168c620cc852cb7b55e2cf0eca5a607cd4e2e
Michael Neale <michael.neale@gmail.com>
2025-04-15 19:48:15 +1000

    another test

--- toolshim-research/run.sh
+++ toolshim-research/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-rm -rf out-chat out-listing out-create-file
-mkdir -p out-chat out-listing out-create-file
+rm -rf out-chat out-listing out-create-file out-create-run
+mkdir -p out-chat out-listing out-create-file out-create-run
 # 
 ./ollama_tester.py --query "what can you do?" --output "out-chat"
 ./ollama_tester.py --query "list files in this dir" --output "out-listing"
 ./ollama_tester.py --query "can you make a file hello.py and put a simple hello world app in it" --output "out-create-file"
+./ollama_tester.py --query "can you make a file boop.js and put a simple hello world app in it, and then run it with node" --output "out-create-run"
