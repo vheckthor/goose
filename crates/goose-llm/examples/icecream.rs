a742a2e6d784fb928d25ce74a926f8bddf9ef80d
Salman Mohammed <smohammed@squareup.com>
2025-05-15 14:57:15 -0700

    icecream example

---
+++ crates/goose-llm/examples/icecream.rs
@@ -0,0 +1,41 @@
+use std::vec;
+
+use anyhow::Result;
+use goose_llm::{
+    completion,
+    types::completion::{CompletionRequest, CompletionResponse},
+    Message, ModelConfig,
+};
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let provider = "databricks";
+    let provider_config = json!({
+        "host": std::env::var("DATABRICKS_HOST").expect("Missing DATABRICKS_HOST"),
+        "token": std::env::var("DATABRICKS_TOKEN").expect("Missing DATABRICKS_TOKEN"),
+    });
+    let model_name = "claude-3-5-haiku";
+    let model_config = ModelConfig::new(model_name.to_string());
+    let system_preamble = "";
+    let messages = vec![Message::user().with_text("what your favorite ice cream")];
+
+    for i in 0..100 {
+        let completion_response: CompletionResponse = completion(CompletionRequest::new(
+            provider.to_string(),
+            provider_config.clone(),
+            model_config.clone(),
+            system_preamble.to_string(),
+            messages.clone(),
+            vec![].clone(),
+        ))
+        .await?;
+        println!(
+            "\n{} Completion: {}\n",
+            i,
+            serde_json::to_string_pretty(&completion_response.message.content)?
+        );
+    }
+
+    Ok(())
+}
